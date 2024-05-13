8285b7d5da852dfe6e24761f7ff1075dccca30b4
Stiven Alexis <stiven22monsalvo@gmail.com>
2024-05-13 14:17:21 -0300

    codigo y test de los diagramas de secuencia

---
+++ TestDiagram/codigoDeHistorias1015.py
@@ -0,0 +1,144 @@
+from abc import ABC
+from datetime import datetime
+from urllib import request
+
+
+class Cuenta(ABC):
+    def __init__(self, nombreDeUsuario, contraseña, formasDeAcceso):
+        self.nombreDeUsuario = nombreDeUsuario
+        self.contraseña = contraseña
+        self.formasDeAcceso = formasDeAcceso
+
+
+
+class Notificacion:
+    def __init__(self, diaProgramado, mesProgramado, anioProgramado, horaProgramado,receptor,titulo,mensaje,emisor):
+        self.emisor = emisor
+        self.receptor = receptor
+        self.titulo = titulo
+        self.mensaje = mensaje
+        self.fechaProgramada = datetime(anioProgramado, mesProgramado, diaProgramado, horaProgramado)
+
+
+
+class Mesa:
+    def __init__(self, periodoDeMesa, profesor, fecha,asignatura,llamado,modalidad,numeroDeAula,hora,rolProfesor,alumnosPresentes,profesoresPresentes):
+        self.periodoDeMesa = periodoDeMesa
+        self.profesor = profesor
+        self.fecha = datetime(fecha)
+        self.asignatura = asignatura
+        self.llamado = llamado
+        self.modalidad = modalidad
+        self.numeroDeAula = numeroDeAula
+        self.hora = hora
+        self.rolProfesor = rolProfesor
+        self.alumnosPresentes = alumnosPresentes
+        self.profesoresPresentes = profesoresPresentes
+
+
+
+class Alerta:
+    def __init__(self,emisor,receptor,titulo,mensaje,):
+        self.emisor = emisor
+        self.receptor = receptor
+        self.titulo = titulo
+        self.mensaje = mensaje
+    
+    respuesta = ""
+    
+    def aceptar(self):
+        self.respuesta= "acepto"
+        
+
+    def rechazar(self,motivo):
+        self.respuesta= "rechazo por "+ motivo 
+        
+
+    def posponer(self,motivo,anio,mes,dia):
+        fechaRespuesta= datetime(anio, mes, dia)
+        fechaRespuestaString=fechaRespuesta.strftime("%Y-%m-%d")
+
+        self.respuesta= "pospongo por "+ motivo + "hasta " + fechaRespuestaString
+              
+
+
+
+class DepartamentoDeCoodinacion(Cuenta):
+ 
+    def intanceMediator(self):
+        mediator = NotificadorMediatorSingleton.getInstance()
+        return mediator
+ 
+    alertasEnviadas = []
+    def newAlerta(self, emisor, receptor, titulo, mensaje):
+        alerta = Alerta(emisor, receptor, titulo, mensaje)
+        self.alertasEnviadas.append(alerta)
+        return alerta 
+        
+   
+
+class NotificadorMediatorSingleton:
+    
+    profesores=[]
+    __instance = None
+
+    def getprofesores(self):
+        return self.profesores
+    
+
+    def addProfesor(self, profesor):
+        profesorExistente=False
+        for profeso in self.profesores:
+            if profeso.dni == profesor.dni:
+                raise Exception("El profesor ya esta en la lista")
+                profesorExistente=True
+    
+        if profesorExistente==False:
+            self.profesores.append(profesor)
+    
+
+
+    @staticmethod
+    def getInstance():
+        if NotificadorMediatorSingleton.__instance == None:
+            NotificadorMediatorSingleton()
+        return NotificadorMediatorSingleton.__instance
+
+    def __init__(self):
+        if NotificadorMediatorSingleton.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            NotificadorMediatorSingleton.__instance = self
+
+    def notificarAlerta(self, alerta):
+        
+        for profesor in self.profesores:
+            profesorEncontrado = False
+            if profesor.dni == alerta.receptor:
+                profesor.recibirAlerta(alerta)
+                profesorEncontrado = True
+        if profesorEncontrado == False:
+            raise Exception("No se encontró ningún profesor con DNI")
+
+
+    def notificar(notificacion):
+        print("Notificando")
+
+class Profesor(Cuenta):
+    def __init__(self, nombre, dni):
+        self.nombre = nombre
+        self.dni = dni
+        
+    bandejaDeAlertas=[]
+
+    def recibirAlerta(self, alerta):
+        self.bandejaDeAlertas.append(alerta)       
+
+    def responderNotificacion(self, alerta= Alerta,respuesta=str,motivo="",anio=0,mes=0,dia=0):
+        if  respuesta== "acepto":
+            alerta.aceptar()
+        elif "rechazo" in respuesta:
+            alerta.rechazar(motivo)
+        elif "pospongo" in respuesta:
+            alerta.posponer(motivo,anio,mes,dia)
+        return alerta
---
+++ TestDiagram/testDiagrama10-15.py
@@ -0,0 +1,94 @@
+import unittest
+from codigoDeHistorias1015 import *
+
+class TestHistorias10_15(unittest.TestCase):
+    
+    def testcreandoAlerta(self):
+        dptoCoor = DepartamentoDeCoodinacion("Juan","juanita22","Google")
+        # Llamada a newAlerta() con los argumentos correctos
+        alerta1 = dptoCoor.newAlerta("departamento",12345678,"Mesa del periodo de abril","se le solicita profesor que acepte de no ser así indique el motivo")
+        # Afirmaciones para verificar los valores de la alerta creada
+        print(alerta1.emisor)
+        self.assertEqual(alerta1.emisor, "departamento")
+        self.assertEqual(alerta1.receptor, 12345678)
+        self.assertEqual(alerta1.titulo, "Mesa del periodo de abril")
+        self.assertEqual(alerta1.mensaje, "se le solicita profesor que acepte de no ser así indique el motivo")
+
+    
+    def testenviarAlertaCorrecta(self):
+       
+        dptoCoor = DepartamentoDeCoodinacion("Juan", "juanita22", "Google")
+        alerta1=dptoCoor.newAlerta("departamento", 12345678, "Mesa del periodo de abril", "se le solicita profesor que acepte, de no ser asi indique el motivo")
+        profesor1=Profesor("Juan Perez",12345678)
+        notificadorMediatorSingleton= dptoCoor.intanceMediator()
+        notificadorMediatorSingleton.profesores.append(profesor1)
+        notificadorMediatorSingleton.notificarAlerta(alerta1)
+        
+        for alerta in profesor1.bandejaDeAlertas:
+            self.assertEqual(alerta.emisor, alerta1.emisor)
+            self.assertEqual(alerta.receptor, alerta1.receptor)
+            self.assertEqual(alerta.titulo, alerta1.titulo)
+            self.assertEqual(alerta.mensaje, alerta1.mensaje)
+    
+    
+        # Crear una alerta con un receptor diferente al DNI del profesor
+    def testenviarAlertaIncorrecta(self):
+       
+        dptoCoor = DepartamentoDeCoodinacion("Juan", "juanita22", "Google")
+        alerta2=dptoCoor.newAlerta("departamento",55555555,  "Mesa del periodo de abril","se le solicita profesor que acepte, de no ser asi indique el motivo")    
+        profesor2=Profesor("Juana Garcia",12345679)
+        profesor2.bandejaDeAlertas.clear()
+        notificadorMediatorSingleton= dptoCoor.intanceMediator()
+        notificadorMediatorSingleton.profesores.append(profesor2)
+        with self.assertRaises(Exception):
+            notificadorMediatorSingleton.notificarAlerta(alerta2)
+        
+        self.assertEqual(len(profesor2.bandejaDeAlertas), 0)
+
+
+    def testAceptarAlerta(self):
+        dptoCoor = DepartamentoDeCoodinacion("Juan", "juanita22", "Google")
+        alerta1=dptoCoor.newAlerta("departamento", 12345678, "Mesa del periodo de abril", "se le solicita profesor que acepte, de no ser asi indique el motivo")
+        profesor1=Profesor("Juan Perez", 12345678)
+        notificadorMediatorSingleton= dptoCoor.intanceMediator()
+        notificadorMediatorSingleton.profesores.append(profesor1)
+        notificadorMediatorSingleton.notificarAlerta(alerta1)
+        profesor1.responderNotificacion(alerta1,"acepto")
+
+        self.assertEqual(len(profesor1.bandejaDeAlertas), 1)
+        self.assertEqual(alerta1.respuesta, "acepto")
+    
+
+    def testRechazarAlerta(self):
+        dptoCoor = DepartamentoDeCoodinacion("Juan", "juanita22", "Google")
+        alerta1=dptoCoor.newAlerta("departamento", 12345678, "Mesa del periodo de abril", "se le solicita profesor que acepte, de no ser asi indique el motivo")
+        profesor1=Profesor("Juan Perez", 12345678)
+        profesor1.bandejaDeAlertas.clear()
+        notificadorMediatorSingleton= dptoCoor.intanceMediator()
+        notificadorMediatorSingleton.profesores.clear()
+        notificadorMediatorSingleton.addProfesor(profesor1)
+        notificadorMediatorSingleton.notificarAlerta(alerta1)
+        profesor1.responderNotificacion(alerta1, "rechazo por", "Matrimonio ")
+
+        self.assertEqual(len(profesor1.bandejaDeAlertas), 1)
+        self.assertEqual(alerta1.respuesta, "rechazo por Matrimonio ")
+
+    def testPosponerAlerta(self):
+        dptoCoor = DepartamentoDeCoodinacion("Juan", "juanita22", "Google")
+        alerta1=dptoCoor.newAlerta("departamento", 12345678, "Mesa del periodo de abril", "se le solicita profesor que acepte, de no ser asi indique el motivo")
+        profesor1=Profesor("Juan Perez", 12345678)
+        profesor1.bandejaDeAlertas.clear()
+        notificadorMediatorSingleton= dptoCoor.intanceMediator()
+        notificadorMediatorSingleton.profesores.clear()
+        notificadorMediatorSingleton.addProfesor(profesor1)
+        notificadorMediatorSingleton.notificarAlerta(alerta1)
+        profesor1.responderNotificacion(alerta1, "pospongo", "Matrimonio ", 2024, 6, 15)
+
+        self.assertEqual(len(profesor1.bandejaDeAlertas), 1)
+        self.assertEqual(alerta1.respuesta, "pospongo por Matrimonio hasta 2024-06-15")  
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+#profesor1.responderNotificacion(alerta1,"rechazo por", " Matrimonio ", 2024,6,15 )
